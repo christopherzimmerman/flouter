ab08e7d24332153c9b713ee70c72dcc3e41b83e8
Chris Zimmerman <chris-zimmerman@live.com>
2019-08-09 12:11:32 -0500

    flake8

---
+++ src/flouter/router.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+    router
+    ~~~~~
+
+    Exposes class for the Flouter library.  Provides an interface
+    for adding routes to the flask application
+
+    :copyright: 2019 Chris Zimmerman
+    :license: BSD-3-Clause
+"""
